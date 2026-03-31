18fdfdeb6671914124f3c8d6314ec9ea1a9749ab
Dvir Dayan <dvirdayan810@gmail.com>
2026-03-31 20:48:38 +0300

    moular esque

--- main.c
+++ main.c
@@ -5,56 +5,36 @@
 
 int main(int argc, char *argv[]) {
     // valid arguments amount passed
-    if (argc == 4)
+    if (argc ==3 || argc == 4)
     {
         char *action = argv[1];
         char *src_path = argv[2];
-        char *dest_path = argv[3];
-
-        if (strcmp(action, "compress") == 0) {
-            if (tsuridity(src_path) && tsuridity(dest_path)) {
-                int flag = tsur_compress(src_path, dest_path);
-                if (flag == 1) {
-                    printf("%s was compressed into %s\n", extract_filename(src_path), extract_filename(dest_path));
-                }
-            }
-        }
-        if (strcmp(action, "decompress") == 0) {
-            if (tsuridity(src_path) && tsuridity(dest_path)) {
-                int flag = tsur_decompress(src_path, dest_path);
-                if (flag == 1) {
-                    printf("%s was decompressed into %s\n", extract_filename(src_path), extract_filename(dest_path));
-                }
-            }
-        }
-    }
-    else if (argc == 3) {
-        char *action = argv[1];
-        char *src_path = argv[2];
-                if (strcmp(action, "compress") == 0) {
-            if (tsuridity(src_path)) {
-                char *dest_path = make_tsur(src_path);
-                if (dest_path != NULL) {
-                    int flag = tsur_compress(src_path, dest_path);
-                    if (flag == 1) {
-                        printf("%s was compressed into %s\n", extract_filename(src_path), extract_filename(dest_path));
-                    }
-                    free(dest_path); // Free the allocated memory
-                }
-            }
-        }
+        char *dest_path = NULL;
+        // Resolve dest_path
+        if (argc == 4) {
+            dest_path = argv[3];
+        } 
+        else if (strcmp(action, "compress") == 0) {
+            dest_path = make_tsur(src_path);
+        } 
         else if (strcmp(action, "decompress") == 0) {
-            if (tsuridity(src_path)) {
-                char *dest_path = kill_tsur(src_path);
-                if (dest_path != NULL) {
-                    int flag = tsur_decompress(src_path, dest_path);
-                    if (flag == 1) {
-                        printf("%s was decompressed into %s\n", extract_filename(src_path), extract_filename(dest_path));
-                    }
-                    free(dest_path); // Free the allocated memory
-                } else {
-                    fprintf(stderr, "Error: Failed to process %s. It may be unreadable or not a valid .tsur file.\n", src_path);
-                }
+            dest_path = kill_tsur(src_path);
+            if (dest_path == NULL) {
+                fprintf(stderr, "Error: Failed to process %s. "
+                    "It may be unreadable or not a valid .tsur file.\n", src_path);
+            }
+        }
+        // Run action
+        if (dest_path != NULL && tsuridity(src_path) && (argc == 4 ? tsuridity(dest_path) : 1)) {
+            if(strcmp(action, "compress") == 0) {
+                tsur_compress(src_path, dest_path);
+            }
+            else if(strcmp(action, "decompress") == 0) {
+                tsur_decompress(src_path, dest_path);
+            }
+            else {
+                printf("Invalid Argument!\n");
+                return -1;
             }
         }
     }
